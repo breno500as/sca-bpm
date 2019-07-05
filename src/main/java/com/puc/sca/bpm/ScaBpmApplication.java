51d4f605f97b9b17538b3ac20de999354dbb7b19
breno.mcosta <breno.mcosta@pbh.gov.br>
2019-07-05 09:23:25 -0300

    configurações iniciais

---
+++ src/main/java/com/puc/sca/bpm/ScaBpmApplication.java
@@ -0,0 +1,15 @@
+package com.puc.sca.bpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ScaBpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaBpmApplication.class, args);
+	}
+
+}
