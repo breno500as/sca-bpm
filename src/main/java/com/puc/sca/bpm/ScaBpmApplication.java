a3a6cc240755e6eb9dae3fc233cac7add96b5eb8
breno500as <breno500as@github.com>
2019-08-24 21:56:12 -0300

    documentação

--- src/main/java/com/puc/sca/bpm/ScaBpmApplication.java
+++ src/main/java/com/puc/sca/bpm/ScaBpmApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Subir no tomcat 9 os wars: flowable-idm.war, flowable-modeler.war
- * URL do flowabble http://localhost:8161/modeler
+ * URL do flowabble http://localhost:8080/flowable-modeler
  * usuário: admin 
  * senha: test
  * @author Breno
