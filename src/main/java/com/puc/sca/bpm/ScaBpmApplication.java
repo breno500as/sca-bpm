45422c4b5d859af961b6e24d4fae9e4e39da52d7
breno500as <breno500as@github.com>
2019-10-20 19:03:23 -0300

    documentação

--- src/main/java/com/puc/sca/bpm/ScaBpmApplication.java
+++ src/main/java/com/puc/sca/bpm/ScaBpmApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 /**
+ * Baixar o flowable: https://www.flowable.org/downloads.html
+ * Descompactar os wars
  * Subir no tomcat 9 os wars: flowable-idm.war, flowable-modeler.war
  * URL do flowabble http://localhost:8080/flowable-modeler
  * usuário: admin 
