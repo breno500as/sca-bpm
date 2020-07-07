6d321efcd0fd81c3c0eb681792aab9f9e504e2c2
breno500as <breno500as@gmail.com>
2020-07-07 19:51:04 -0300

    formatação

--- src/main/java/com/puc/sca/bpm/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/puc/sca/bpm/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -20,7 +20,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
 		final ExceptionResponse exceptionResponse = 
 				new ExceptionResponse(
-						 LocalDateTime.now(),
+						LocalDateTime.now(),
 						request.getDescription(false),
 						ex.getMessage());
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -30,7 +30,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(ResourceNotFoundException ex, WebRequest request) {
 		final ExceptionResponse exceptionResponse = 
 				new ExceptionResponse(
-						 LocalDateTime.now(),
+						LocalDateTime.now(),
 						request.getDescription(false),
 						ex.getMessage());
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
@@ -41,7 +41,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	public final ResponseEntity<ExceptionResponse> handleExpectationFailedExceptions(BpmException ex, WebRequest request) {
 		final ExceptionResponse exceptionResponse = 
 				new ExceptionResponse(
-						 LocalDateTime.now(),
+						LocalDateTime.now(),
 						request.getDescription(false),
 						ex.getMessages());
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.EXPECTATION_FAILED);
